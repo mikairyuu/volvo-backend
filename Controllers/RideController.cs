c335a34a46a23304f83f637466b3a1fa51366b3e
Bengeta <inovozhilov08@gmail.com>
2021-10-23 23:03:32 +1000

    Join Ride By Id

--- Controllers/RideController.cs
+++ Controllers/RideController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using System;
 using volvo_backend.Models;
-using volvo_backend.Services;
 using volvo_backend.Utils;
 
 namespace volvo_backend.Controllers
@@ -12,32 +11,35 @@ namespace volvo_backend.Controllers
     public class RideController : ControllerBase
     {
         [HttpPost("join")]
-        public ActionResult<Ride> JoinRideById([FromQuery(Name = "RideId")] int rideId, [FromQuery(Name = "UserId")] int userId)
+        public ActionResult<RouteModel> JoinRideById([FromQuery(Name = "RideId")] int routeId, [FromQuery(Name = "UserId")] int userId)
         {
-            var ride = new Ride();
+            RouteModel ride = null;
             var dbase = new DBManager();
-            var cmd = new MySqlCommand($"select * from eventtable where event_id = @id");
-            cmd.Parameters.AddWithValue("@id", rideId);
-            var reader = dbase.GetReader(cmd);
-            if (!reader.Read()) return null;
-            var routeId = reader.GetInt32("route_id");
-            dbase.CloseReader();
-            cmd = new MySqlCommand($"select * from routetable where route_id =  @id");
+            var cmd = new MySqlCommand(
+                $"select * from routetable where route_id =  @id;" +
+                $"update routetable Set route_visited =route_visited+1 where route_id = @id ;");
             cmd.Parameters.AddWithValue("@id", routeId);
-            reader = dbase.GetReader(cmd);
-            if (reader.Read())
-                ride.route = new RouteModel()
-                {
-                    Id = reader.GetInt32("route_id"),
-                    Img = reader.GetString("route_img"),
-                    Distance = reader.GetInt32("route_distance"),
-                    Title = reader.GetString("route_name"),
-                    Description = reader.GetString("route_description"),
-                    LastUsedAtTS = ((DateTimeOffset) reader.GetMySqlDateTime("route_last_date").Value)
-                        .ToUnixTimeSeconds().ToString()
-                };
+            var reader = dbase.GetReader(cmd);
+            if (!reader.Read()) return BadRequest();
+            ride = new RouteModel()
+            {
+                Id = reader.GetInt32("route_id"),
+                Img = reader.GetString("route_img"),
+                Distance = reader.GetInt32("route_distance"),
+                Title = reader.GetString("route_name"),
+                Description = reader.GetString("route_description"),
+                LastUsedAtTS = ((DateTimeOffset)reader.GetMySqlDateTime("route_last_date").Value)
+                   .ToUnixTimeSeconds().ToString()
+            };
+            dbase.CloseReader();
+            cmd = new MySqlCommand(
+                $"INSERT INTO eventuser(user_id, route_id) VALUES (@user_id,@route_id);");
+            cmd.Parameters.AddWithValue("@route_id", routeId);
+            cmd.Parameters.AddWithValue("@user_id", userId);
+            dbase.InsertCommand(cmd);
             dbase.CloseConnection();
             return ride;
+
         }
 
     }
