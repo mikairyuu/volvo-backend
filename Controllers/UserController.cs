8d9b3680649f3149f9e9b61a88a2367267aae830
Bengeta <inovozhilov08@gmail.com>
2021-10-23 23:34:34 +1000

    Get all routes by user id

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
@@ -13,8 +14,29 @@ namespace volvo_backend.Controllers
         [HttpGet("ride/get")]
         public ActionResult<List<RouteModel>> GetRideById([FromQuery(Name = "UserId")] int userId)
         {
-            //TODO
-            return null;
+            var result = new List<UserInfo>();
+            var dbase = new DBManager();
+            var cmd = new MySqlCommand(
+                "SELECT * FROM ( SELECT `route_id` FROM `eventuser` WHERE user_id = @id ) " +
+                "AS fr JOIN routetable on routetable.route_id = fr.route_id");
+            cmd.Parameters.AddWithValue("@id", userId);
+            var reader = dbase.GetReader(cmd);
+            var list = new List<RouteModel>();
+            while (reader.Read())
+            {
+                list.Add ( new RouteModel
+                {
+                    Id = reader.GetInt32("route_id"),
+                    Img = reader.GetString("route_img"),
+                    Distance = reader.GetInt32("route_distance"),
+                    Title = reader.GetString("route_name"),
+                    Description = reader.GetString("route_description"),
+                    UseCount = reader.GetInt32("route_visited"),
+                    LastUsedAtTS = ((DateTimeOffset)reader.GetMySqlDateTime("route_last_date").Value)
+                        .ToUnixTimeSeconds().ToString()
+                });
+            }
+            return list;
         }
 
         [HttpGet("rating/get")]
