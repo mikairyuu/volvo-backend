a19938198f5342edeb9cb8c7d44a0f8dd834e4db
Bengeta <inovozhilov08@gmail.com>
2021-10-24 12:00:05 +1000

    fix get all routes by user id (now with many imgs))

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -18,24 +18,30 @@ namespace volvo_backend.Controllers
             var dbase = new DBManager();
             var cmd = new MySqlCommand(
                 "SELECT * FROM ( SELECT `route_id` FROM `eventuser` WHERE user_id = @id ) " +
-                "AS fr JOIN routetable on routetable.route_id = fr.route_id");
+                "AS fr JOIN routetable on routetable.route_id = fr.route_id " +
+                "JOIN imagetable on imagetable.route_id=fr.route_id");
             cmd.Parameters.AddWithValue("@id", userId);
             var reader = dbase.GetReader(cmd);
             var list = new List<RouteModel>();
+            var prevId = -1;
             while (reader.Read())
             {
-                list.Add(new RouteModel
-                {
-                    Id = reader.GetInt32("route_id"),
-                    Img = new List<string> {reader.GetString("route_img")},
-                    Distance = reader.GetInt32("route_distance"),
-                    Title = reader.GetString("route_name"),
-                    Description = reader.GetString("route_description"),
-                    UseCount = reader.GetInt32("route_visited"),
-                    LastUsedAtTS = ((DateTimeOffset) reader.GetMySqlDateTime("route_last_date").Value)
-                        .ToUnixTimeSeconds().ToString()
-                });
+                if (reader.GetInt32("route_id") != prevId)
+                    list.Add(new RouteModel
+                    {
+                        Id = reader.GetInt32("route_id"),
+                        Img = new List<string> { reader.GetString("Img") },
+                        Distance = reader.GetInt32("route_distance"),
+                        Title = reader.GetString("route_name"),
+                        Description = reader.GetString("route_description"),
+                        UseCount = reader.GetInt32("route_visited"),
+                        LastUsedAtTS = ((DateTimeOffset)reader.GetMySqlDateTime("route_last_date").Value)
+                            .ToUnixTimeSeconds().ToString()
+                    });
+                else
+                    list[list.Count - 1].Img.Add(reader.GetString("Img"));
             }
+            dbase.CloseConnection();
 
             return list;
         }
